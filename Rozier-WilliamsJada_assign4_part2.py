89383e6bf7a54bfb37d1400f8ff2c717a59fe0ed
Cosmocs <101135810+Cosmocs@users.noreply.github.com>
2023-02-26 17:58:49 -0500

    Update Rozier-WilliamsJada_assign4_part2.py

--- Rozier-WilliamsJada_assign4_part2.py
+++ Rozier-WilliamsJada_assign4_part2.py
@@ -1,14 +1,14 @@
 import random
 
-player = False
-
-while player == False:
+rounds = int(input("How many rounds would you like? "))
+for i in range(1, rounds):
 #set player to True
     print("Let's Play Rock, Paper, Scissors, Lizard, Spock!")
+   
     player = input("Enter a choice (rock, paper, scissors, lizzard, spock): ")
     choices = ["rock", "paper", "scissors", "lizzard", "spock"]
     computer_action = random.choice(choices)
-    print(f"\nYou chose {user_choice}, computer chose {computer_action}.\n")
+    print(f"\nYou chose {player}, computer chose {computer_action}.\n")
 
     if player == computer_action:
         print("It's a tie!")
@@ -32,11 +32,11 @@ while player == False:
             print("You win!", player, "poisons", computer_action)
         else:
             print("You lose!", computuer_action, "poisons", player)
-    elif player == "spock"
-        if computer_action == "rock"
+    elif player == "spock":
+        if computer_action == "rock":
             print("You win!", player, "crushes", computer_action)
         else:
-             print("You loose!", computer_action, "crushes", player)
+             print("You lose!", computer_action, "crushes", player)
     else:
         print("That's not a valid play. Check your spelling!")
     #player was set to True, but we want it to be False so the loop continues
