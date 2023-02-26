4fab9527ed20055eb44937821e8438569603ec57
Cosmocs <101135810+Cosmocs@users.noreply.github.com>
2023-02-26 13:32:40 -0500

    Update Rozier-WilliamsJada_assign4_part2.py

--- Rozier-WilliamsJada_assign4_part2.py
+++ Rozier-WilliamsJada_assign4_part2.py
@@ -5,3 +5,23 @@ while True:
 
     choices = ["rock", "paper", "scissors", "lzzard", "spock"]
     computer_action = random.choice(choices)
+    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+
+    if user_choice == computer_action:
+        print(f"Both players selected {user_choice}. It's a tie!")
+    elif user_choice == "rock":
+        if computer_action == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_choice == "paper":
+        if computer_action == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_choice == "scissors":
+        if computer_action == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+
