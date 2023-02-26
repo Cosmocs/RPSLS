2310f8dcc6b76b762d5443a2367f75b5b5b5ab72
Cosmocs <101135810+Cosmocs@users.noreply.github.com>
2023-02-26 13:31:02 -0500

    Update Rozier-WilliamsJada_assign4_part2.py

--- Rozier-WilliamsJada_assign4_part2.py
+++ Rozier-WilliamsJada_assign4_part2.py
@@ -1,6 +1,7 @@
 import random
 
-user_choice = input("Enter a choice (rock, paper, scissors, lizzard, spock): ")
+while True:
+    user_choice = input("Enter a choice (rock, paper, scissors, lizzard, spock): ")
 
-choices = ["rock", "paper", "scissors", "lzzard", "spock"]
-computer_action = random.choice(choices)
+    choices = ["rock", "paper", "scissors", "lzzard", "spock"]
+    computer_action = random.choice(choices)
